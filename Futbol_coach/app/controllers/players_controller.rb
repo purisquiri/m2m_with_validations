857e571fd34c6a3c553eed0ecdd8b92f5e45d5ec
BHessel <hesselfitness@gmail.com>
2020-11-04 17:03:06 -0500

    finished players, validation, errors

--- Futbol_coach/app/controllers/players_controller.rb
+++ Futbol_coach/app/controllers/players_controller.rb
@@ -1,13 +1,52 @@
 class PlayersController < ApplicationController
-  def index
+  
+  def create
+    @player = Player.new(player_params)
+    if @player.valid?
+      @player.save
+      redirect_to player_path(@player)
+    else
+      render :new
+    end
+  end
+  
+  def new
+    @player = Player.new
   end
 
   def show
-  end
-
-  def new
+    find_player
   end
 
   def edit
+    find_player
   end
+
+  def update
+    find_player
+    @player.update(player_params)
+    redirect_to player_path(@player)
+  end
+
+  def destroy
+    find_player.delete
+    redirect_to players_path
+  end
+
+  def index
+    @players = Player.all    
+  end
+
+
+
+  private
+  
+  def player_params
+    params.require(:player).permit(:name, :phone_number, :email, :player_number, :club_id)
+  end
+
+  def find_player
+    @player = Player.find(params[:id])
+  end
+
 end
--- Futbol_coach/app/models/player.rb
+++ Futbol_coach/app/models/player.rb
@@ -1,3 +1,11 @@
 class Player < ApplicationRecord
+    
+    validates :name, :email, uniqueness: {case_sensitive: false}
+    validates :phone_number, length: {is: 10}
+    validates :phone_number, :email, :player_number, :name, presence: true
+    validates :player_number, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 99}
+    
     belongs_to :club
+
 end
+
